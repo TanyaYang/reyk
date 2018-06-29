e4c6c828318094357956079c0e5b9d06d5b4da30
ytyang <ytyang@wwj.org>
2018-06-29 11:11:04 +0800

    web_home
    首页基础框架，纯静态

---
+++ web/js/reyk.js
@@ -0,0 +1,26 @@
+
+var swiper = new Swiper('#swiper1', {
+      spaceBetween: 30,
+      effect: 'fade',
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+
+var swiper = new Swiper('#swiper2', {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+    });
